6b3362f79c224ae7d10d279fe0437c1fb7d800c9
Katalin Molnar <molnarlin@gmail.com>
2025-04-05 12:26:27 +0000

    network request added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,10 @@ import css from "./App.module.css";
 // import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
-import Content from "./components/Content";
-import ContentHooks from "./components/ContentHooks";
+//import Content from "./components/Content";
+// import ContentHooks from "./components/ContentHooks";
+// import ContentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
       <NavBarForm />
       <Sidebar />
       {/* <Content /> */}
-      <ContentHooks />
+      {/* <ContentHooks /> */}
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
     </div>
   );
 }
